666303f8abc99aa7c95a8ae2c53813022708f2c1
Daniil Iaitskov <dyaitskov@gmail.com>
2025-07-06 16:28:33 -0800

    master trace unpacking script

--- unpack-git-repo.sh
+++ unpack-git-repo.sh
@@ -4,10 +4,13 @@
 # The script does unpacking objects.
 
 set -e
+set -x
 
+ls -la
+rm -rf unpacked
 git init unpacked
 find .git -name '*.pack' | while read p ; do git -C unpacked unpack-objects < $p ; done
 git -C unpacked checkout -b master $(git log -1 | grep -oE '[a-f0-9]{40}')
 mv .git .git.bak
-rm -rf unpacked
 mv unpacked/.git .git
+rm -rf unpacked
